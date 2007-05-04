cc88068650a37d7960e745c5012ee7dfd898b5f0
gas <gas@88262fed-822f-0410-898e-89d428b5ecb3>
2007-05-04 20:06:22 +0000

    more package changes

--- trunk/extensions/webcontainer/src/main/java/com/thoughtworks/waffle/groovy/ViewSuffixNodeBuilder.java
+++
@@ -1,8 +0,0 @@
-package org.codehaus.waffle.groovy;
-
-import org.nanocontainer.webcontainer.PicoContextHandler;
-
-public class ViewSuffixNodeBuilder extends Object {
-    public ViewSuffixNodeBuilder(PicoContextHandler context, String suffix) {
-    }
-}
