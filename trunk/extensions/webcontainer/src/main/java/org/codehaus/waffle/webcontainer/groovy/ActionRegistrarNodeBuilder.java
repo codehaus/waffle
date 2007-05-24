ea7ff4d560fe2ce1548add7b4d6340b1e52e0acd
mauro <mauro@88262fed-822f-0410-898e-89d428b5ecb3>
2007-05-24 07:43:12 +0000

    Fixed copyright notice.

--- trunk/extensions/webcontainer/src/main/java/org/codehaus/waffle/webcontainer/groovy/ActionRegistrarNodeBuilder.java
+++ trunk/extensions/webcontainer/src/main/java/org/codehaus/waffle/webcontainer/groovy/ActionRegistrarNodeBuilder.java
@@ -1,5 +1,6 @@
 /*****************************************************************************
- * Copyright (C) NanoContainer Organization. All rights reserved.            *
+ * Copyright (C) 2005,2006 Michael Ward                                      *
+ * All rights reserved.                                                      *
  * ------------------------------------------------------------------------- *
  * The software in this package is published under the terms of the BSD      *
  * style license a copy of which has been included with this distribution in *
