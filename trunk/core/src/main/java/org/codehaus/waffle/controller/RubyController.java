d9883df14911504fb0b98af1557bf3337850da60
mward <mward@88262fed-822f-0410-898e-89d428b5ecb3>
2007-06-05 23:03:22 +0000

    RubyController can handle action methods with arguments (following waffle '|' way)

--- trunk/core/src/main/java/org/codehaus/waffle/controller/RubyController.java
+++ trunk/core/src/main/java/org/codehaus/waffle/controller/RubyController.java
@@ -2,10 +2,15 @@ package org.codehaus.waffle.controller;
 
 import org.jruby.Ruby;
 import org.jruby.javasupport.JavaUtil;
+import org.jruby.javasupport.JavaEmbedUtils;
 import org.jruby.runtime.builtin.IRubyObject;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  * This is a wrapper for the underlying ruby script
@@ -26,10 +31,27 @@ public class RubyController {
         return rubyObject;
     }
 
-    // todo need to ensure this doesn't allow non-public methods to be called
+    // todo need to ensure this doesn't allow non-public methods to be called ... NEED test in general
     public Object execute(HttpServletRequest request, HttpServletResponse response) {
         Ruby runtime = rubyObject.getRuntime();
-        IRubyObject result = rubyObject.callMethod(runtime.getCurrentContext(), methodName);
+        IRubyObject result;
+
+        String[] strings = methodName.split("\\|");
+
+        if(strings.length == 0) {
+            result = rubyObject.callMethod(runtime.getCurrentContext(), methodName);
+        } else {
+            Iterator<String> iterator = Arrays.asList(strings).iterator();
+
+            methodName = iterator.next();
+            List<IRubyObject> arguments = new ArrayList<IRubyObject>();
+
+            while (iterator.hasNext()) {
+                arguments.add(JavaEmbedUtils.javaToRuby(runtime, iterator.next()));
+            }
+            
+            result = rubyObject.callMethod(runtime.getCurrentContext(), methodName, arguments.toArray(new IRubyObject[0]));
+        }
 
         return JavaUtil.convertRubyToJava(result);
     }
