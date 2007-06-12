dfa05e00ee27150663bf47cb63b98e98df4d422d
mward <mward@88262fed-822f-0410-898e-89d428b5ecb3>
2007-06-12 22:59:27 +0000

    moved waffle.rb into org.codehaus.waffle package.  Removed string.rb since it is no longer needed.

--- trunk/core/src/main/ruby/string.rb
+++
@@ -1,8 +0,0 @@
-# Following was copied from RoR (Inflector module)...
-def String.camelize(lower_case_and_underscored_word, first_letter_in_uppercase = true)
-  if first_letter_in_uppercase
-    lower_case_and_underscored_word.to_s.gsub(/\/(.?)/) { "::" + $1.upcase }.gsub(/(^|_)(.)/) { $2.upcase }
-  else
-    lower_case_and_underscored_word.first + camelize(lower_case_and_underscored_word)[1..-1]
-  end
-end
