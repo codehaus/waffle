5e0cb228b0eabc6d5fdbe2739bf6c4b5715e4873
mauro <mauro@88262fed-822f-0410-898e-89d428b5ecb3>
2007-11-03 11:46:15 +0000

    Migrated controller and i18n unit tests to jMock 2.

--- trunk/core/src/test/java/org/codehaus/waffle/controller/RubyControllerDefinitionFactoryTest.java
+++ trunk/core/src/test/java/org/codehaus/waffle/controller/RubyControllerDefinitionFactoryTest.java
@@ -1,34 +1,60 @@
 package org.codehaus.waffle.controller;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.codehaus.waffle.context.ContextContainer;
 import org.codehaus.waffle.context.RequestLevelContainer;
-import org.jmock.Mock;
-import org.jmock.MockObjectTestCase;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
 import org.jruby.Ruby;
 import org.jruby.javasupport.JavaUtil;
 import org.jruby.runtime.builtin.IRubyObject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
-public class RubyControllerDefinitionFactoryTest extends MockObjectTestCase {
 
-    public void testFindControllerHandlesIRubyObject() {
+/**
+ * 
+ * @author Michael Ward
+ * @author Mauro Talevi
+ */
+@RunWith(JMock.class)
+public class RubyControllerDefinitionFactoryTest {
+
+    private Mockery mockery = new Mockery();
+
+    @Test
+    public void canHandleIRubyObject() {
         RubyControllerDefinitionFactory factory = new RubyControllerDefinitionFactory(null, null, null);
 
-        Mock mockContextContainer = mock(ContextContainer.class);
-        IRubyObject rubyObject = JavaUtil.convertJavaToRuby(Ruby.getDefaultInstance(), "Hello From Ruby");
-        mockContextContainer.expects(once()).method("getComponentInstance").with(eq("foobar")).will(returnValue(rubyObject));
-        ContextContainer contextContainer = (ContextContainer) mockContextContainer.proxy();
+        final ContextContainer contextContainer = mockery.mock(ContextContainer.class);
+        mockery.checking(new Expectations() {
+            {
+                IRubyObject rubyObject = JavaUtil.convertJavaToRuby(Ruby.getDefaultInstance(), "Hello From Ruby");
+                one(contextContainer).getComponentInstance("foobar");
+                will(returnValue(rubyObject));
+            }
+        });
         RequestLevelContainer.set(contextContainer);
 
         Object controller = factory.findController("foobar", null);
         assertTrue(controller instanceof RubyController);
     }
 
-    public void testFindControllerHandlesNonRubyObjects() {
+    @Test
+    public void canHandleNonRubyObjects() {
         RubyControllerDefinitionFactory factory = new RubyControllerDefinitionFactory(null, null, null);
 
-        Mock mockContextContainer = mock(ContextContainer.class);
-        mockContextContainer.expects(once()).method("getComponentInstance").with(eq("foobar")).will(returnValue("Pojo"));
-        ContextContainer contextContainer = (ContextContainer) mockContextContainer.proxy();
+        final ContextContainer contextContainer = mockery.mock(ContextContainer.class);
+        mockery.checking(new Expectations() {
+            {
+                one(contextContainer).getComponentInstance("foobar");
+                will(returnValue("Pojo"));
+            }
+        });
         RequestLevelContainer.set(contextContainer);
 
         Object controller = factory.findController("foobar", null);
