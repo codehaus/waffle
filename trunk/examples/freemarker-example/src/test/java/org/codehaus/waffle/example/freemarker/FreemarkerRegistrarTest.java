678ffe42c94382929ca5295012205b3f57952144
mauro <mauro@88262fed-822f-0410-898e-89d428b5ecb3>
2007-11-03 13:49:30 +0000

    Added examples of usage of PicoRegistrarMockery.

--- trunk/examples/freemarker-example/src/test/java/org/codehaus/waffle/example/freemarker/FreemarkerRegistrarTest.java
+++ trunk/examples/freemarker-example/src/test/java/org/codehaus/waffle/example/freemarker/FreemarkerRegistrarTest.java
@@ -1,6 +1,6 @@
 package org.codehaus.waffle.example.freemarker;
 
-import org.codehaus.waffle.example.freemarker.FreemarkerRegistrar;
+import org.codehaus.waffle.mock.PicoRegistrarMockery;
 import org.codehaus.waffle.registrar.mock.AbstractPicoRegistrarTestCase;
 
 public class FreemarkerRegistrarTest extends AbstractPicoRegistrarTestCase {
@@ -8,4 +8,10 @@ public class FreemarkerRegistrarTest extends AbstractPicoRegistrarTestCase {
     public void testConfiguration() {
         assertConfiguration(FreemarkerRegistrar.class);
     }
+
+    public void testConfigurationWithMockery() {
+        PicoRegistrarMockery mockery = new PicoRegistrarMockery();
+        mockery.assertConfiguration(FreemarkerRegistrar.class);
+    }
+
 }
--- trunk/examples/simple-example/src/test/java/org/codehaus/waffle/example/simple/SimpleRegistrarTest.java
+++ trunk/examples/simple-example/src/test/java/org/codehaus/waffle/example/simple/SimpleRegistrarTest.java
@@ -1,6 +1,6 @@
 package org.codehaus.waffle.example.simple;
 
-import org.codehaus.waffle.example.simple.SimpleRegistrar;
+import org.codehaus.waffle.mock.PicoRegistrarMockery;
 import org.codehaus.waffle.registrar.mock.AbstractPicoRegistrarTestCase;
 
 public class SimpleRegistrarTest extends AbstractPicoRegistrarTestCase {
@@ -8,4 +8,10 @@ public class SimpleRegistrarTest extends AbstractPicoRegistrarTestCase {
     public void testConfiguration() {
         assertConfiguration(SimpleRegistrar.class);
     }
+
+    public void testConfigurationWithMockery() {
+        PicoRegistrarMockery mockery = new PicoRegistrarMockery();
+        mockery.assertConfiguration(SimpleRegistrar.class);
+    }
+
 }
