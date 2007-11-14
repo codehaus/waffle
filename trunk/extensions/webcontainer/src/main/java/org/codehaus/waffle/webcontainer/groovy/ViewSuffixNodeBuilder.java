319006b6f499084a32d285e142db3ebfa93a106e
mward <mward@88262fed-822f-0410-898e-89d428b5ecb3>
2007-11-14 13:40:36 +0000

    removing webcontainer module from project (after discussion with paul) ... could potentially be revisited in the future

--- trunk/extensions/webcontainer/src/main/java/org/codehaus/waffle/webcontainer/groovy/ViewSuffixNodeBuilder.java
+++
@@ -1,8 +0,0 @@
-package org.codehaus.waffle.webcontainer.groovy;
-
-import org.nanocontainer.webcontainer.PicoContext;
-
-public class ViewSuffixNodeBuilder extends Object {
-    public ViewSuffixNodeBuilder(PicoContext context, String suffix) {
-    }
-}
