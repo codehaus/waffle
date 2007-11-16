4164cc99cfb77bd2b2ad5556b1a58c7f9e528a26
mauro <mauro@88262fed-822f-0410-898e-89d428b5ecb3>
2007-11-16 08:55:53 +0000

    Added ConsoleMonitor. Renamed writing monitor methods to monitorLevels() and monitorMessages() for better clarity.

--- examples/simple-example/src/main/java/org/codehaus/waffle/example/simple/SimpleMonitor.java
+++ examples/simple-example/src/main/java/org/codehaus/waffle/example/simple/SimpleMonitor.java
@@ -11,8 +11,8 @@ public class SimpleMonitor extends CommonsLoggingMonitor {
      * We want to shouw that we can change all monitor levels to INFO
      */
     @Override
-    protected Map<String, Level> eventLevels(){
-        Map<String, Level> levels = super.eventLevels();
+    protected Map<String, Level> monitorLevels(){
+        Map<String, Level> levels = super.monitorLevels();
         for ( String key : levels.keySet() ){
             levels.put(key, Level.INFO);
         }
