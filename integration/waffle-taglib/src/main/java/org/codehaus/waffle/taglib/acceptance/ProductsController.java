015d87581909ec5035f77577fcb5d61febfb5529
fabio.kung <fabio.kung@88262fed-822f-0410-898e-89d428b5ecb3>
2007-12-14 20:28:20 +0000

    messages tag together with the acceptance tests

--- integration/waffle-taglib/src/main/java/org/codehaus/waffle/taglib/acceptance/ProductsController.java
+++ integration/waffle-taglib/src/main/java/org/codehaus/waffle/taglib/acceptance/ProductsController.java
@@ -4,6 +4,7 @@ import org.codehaus.waffle.action.annotation.ActionMethod;
 import org.codehaus.waffle.validation.ErrorsContext;
 import org.codehaus.waffle.validation.FieldErrorMessage;
 import org.codehaus.waffle.validation.GlobalErrorMessage;
+import org.codehaus.waffle.i18n.MessagesContext;
 
 /**
  * @Author Fabio Kung
@@ -13,5 +14,10 @@ public class ProductsController {
     @ActionMethod(parameters = {"name", "price"})
     public void add(String name, Double price) {
         // has validation errors...
+    }
+
+    public void showMessages(MessagesContext messages) {
+        messages.addMessage("success", "Successful!");
+        messages.addMessage("operation", "Operation done.");
     }
 }
