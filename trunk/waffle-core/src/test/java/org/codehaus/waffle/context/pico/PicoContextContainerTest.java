e7846af165cf70371b13c56cd84c58417590b292
mward <mward@88262fed-822f-0410-898e-89d428b5ecb3>
2008-01-17 17:16:09 +0000

    WAFFLE-57 : now wrapping any pico exception thrown in a WaffleException.

---
+++ trunk/waffle-core/src/test/java/org/codehaus/waffle/context/pico/PicoContextContainerTest.java
@@ -0,0 +1,42 @@
+package org.codehaus.waffle.context.pico;
+
+import org.codehaus.waffle.WaffleException;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.fail;
+import org.junit.runner.RunWith;
+import org.picocontainer.MutablePicoContainer;
+import org.picocontainer.PicoException;
+
+/**
+ *
+ * @author Michael Ward
+ */
+@RunWith(JMock.class)
+public class PicoContextContainerTest {
+    private Mockery mockery = new Mockery();
+
+    @SuppressWarnings({"ThrowableInstanceNeverThrown"})
+    @Test
+    public void getComponentInstanceShouldWrapAnyPicoException() {
+        final MutablePicoContainer mutablePicoContainer = mockery.mock(MutablePicoContainer.class);
+        mockery.checking(new Expectations() {
+            {
+                one(mutablePicoContainer).getComponentInstance("foo");
+                will(throwException(new PicoException("fake from test") {}));
+            }
+        });
+
+        PicoContextContainer picoContextContainer = new PicoContextContainer(mutablePicoContainer);
+
+        try {
+            picoContextContainer.getComponentInstance("foo");
+            fail("WaffleException expected");
+        } catch (WaffleException expected) {
+            Assert.assertEquals("Unable to construct component 'foo'.", expected.getMessage());
+        }
+    }
+}
