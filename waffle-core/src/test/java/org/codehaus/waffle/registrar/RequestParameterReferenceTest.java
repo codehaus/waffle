b2f6cb2a8e7f0eb5eeda2ffdf50dcc3c181f055c
mward <mward@88262fed-822f-0410-898e-89d428b5ecb3>
2008-01-17 02:52:06 +0000

    WAFFLE-55 : updated RequestParameterReference to allow for a default value (useful since request parameter values are being retreived).

--- waffle-core/src/test/java/org/codehaus/waffle/registrar/RequestParameterReferenceTest.java
+++ waffle-core/src/test/java/org/codehaus/waffle/registrar/RequestParameterReferenceTest.java
@@ -10,5 +10,14 @@ public class RequestParameterReferenceTest {
     public void canConstructInstanceFromParameterMethod() {
         RequestParameterReference reference = requestParameter("foo");
         assertEquals("foo", reference.getKey());
+        assertEquals(null, reference.getDefaultValue()); // default is null
     }
+
+    @Test
+    public void canAllowForDefaultValueWhenParameterValueIsNull() {
+        RequestParameterReference reference = requestParameter("foo", "bar");
+        assertEquals("foo", reference.getKey());
+        assertEquals("bar", reference.getDefaultValue());
+    }
+
 }
