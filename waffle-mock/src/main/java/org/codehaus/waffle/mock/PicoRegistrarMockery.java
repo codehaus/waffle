4955b1d5779fc97f81030ec8664a3bc8ce07e600
mauro <mauro@88262fed-822f-0410-898e-89d428b5ecb3>
2008-01-18 15:16:56 +0000

    WAFFLE-56:  Added instance of ParameterResolver to PicoRegistrarMockery.  Added unit test.

--- waffle-mock/src/main/java/org/codehaus/waffle/mock/PicoRegistrarMockery.java
+++ waffle-mock/src/main/java/org/codehaus/waffle/mock/PicoRegistrarMockery.java
@@ -1,13 +1,17 @@
 package org.codehaus.waffle.mock;
 
+import org.codehaus.waffle.bind.DefaultStringTransmuter;
+import org.codehaus.waffle.bind.ognl.OgnlValueConverterFinder;
 import org.codehaus.waffle.context.ContextContainer;
 import org.codehaus.waffle.context.pico.PicoContextContainer;
 import org.codehaus.waffle.context.pico.PicoLifecycleStrategy;
 import org.codehaus.waffle.registrar.Registrar;
+import org.codehaus.waffle.registrar.pico.DefaultParameterResolver;
+import org.codehaus.waffle.registrar.pico.ParameterResolver;
 import org.codehaus.waffle.registrar.pico.PicoRegistrar;
 import org.picocontainer.MutablePicoContainer;
-import org.picocontainer.monitors.NullComponentMonitor;
 import org.picocontainer.defaults.LifecycleStrategy;
+import org.picocontainer.monitors.NullComponentMonitor;
 
 /**
  * Registrar Mockery that uses PicoContainer as the ContextContainer
@@ -22,6 +26,7 @@ public class PicoRegistrarMockery extends AbstractRegistrarMockery {
 
     protected Registrar createRegistrar(ContextContainer container) {
         LifecycleStrategy lifecycleStrategy = new PicoLifecycleStrategy(new NullComponentMonitor());
-        return new PicoRegistrar((MutablePicoContainer) container.getDelegate(), null, lifecycleStrategy, getRegistrarMonitor(container));
+        ParameterResolver parameterResolver = new DefaultParameterResolver(new DefaultStringTransmuter(new OgnlValueConverterFinder()));
+        return new PicoRegistrar((MutablePicoContainer) container.getDelegate(), parameterResolver, lifecycleStrategy, getRegistrarMonitor(container));
     }
 }
