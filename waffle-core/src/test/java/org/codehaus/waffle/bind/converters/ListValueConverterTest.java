6e40679cf4f51ec2d3596cf134f568bdde2e1b9f
mauro <mauro@88262fed-822f-0410-898e-89d428b5ecb3>
2008-04-20 22:27:30 +0000

    Refactored ListValueConverter to use NumberFormat to parse numbers.

--- waffle-core/src/test/java/org/codehaus/waffle/bind/converters/ListValueConverterTest.java
+++ waffle-core/src/test/java/org/codehaus/waffle/bind/converters/ListValueConverterTest.java
@@ -25,8 +25,8 @@ public class ListValueConverterTest {
 
     private static final List<Integer> INTEGERS = asList(1,2,3);
     private static final List<Long> LONGS = asList(1L,2L,3L);
-    private static final List<Float> FLOATS = asList(0.1f,0.2f,0.3f);
     private static final List<Double> DOUBLES = asList(0.1d,0.2d,0.3d);
+    private static final List<Float> FLOATS = asList(0.1f,0.2f,0.3f);
     
     private MessageResourcesConfiguration configuration = new MessageResourcesConfiguration(){
 
@@ -49,14 +49,15 @@ public class ListValueConverterTest {
     public void canConvert() throws OgnlException {
         DefaultMessageResources resources = new DefaultMessageResources(configuration);
         ListValueConverter converter = new ListValueConverter(resources);
-        assertCanConvertValueToList(converter, INTEGERS, "1,2,3");
-        assertCanConvertValueToList(converter, LONGS, "1,2,3");
-        assertCanConvertValueToList(converter, FLOATS, "0.1,0.2,0.3");
-        assertCanConvertValueToList(converter, DOUBLES, "0.1,0.2,0.3");
+        assertCanConvertValueToList(converter, INTEGERS, "1,2,3", Integer.class);
+        assertCanConvertValueToList(converter, LONGS, "1,2,3", Long.class);
+        assertCanConvertValueToList(converter, DOUBLES, "0.1,0.2,0.3", Double.class);
+        assertCanConvertValueToList(converter, FLOATS, "0.1,0.2,0.3", Float.class);
     }
 
-    private void assertCanConvertValueToList(ListValueConverter converter, List<?> list, String value) {
+    private void assertCanConvertValueToList(ListValueConverter converter, List<?> list, String value, Class<? extends Number> type) {
         assertEquals(list.toString(), converter.convertValue("property-name", value, List.class).toString());
+        assertTrue(list.get(0).getClass().isAssignableFrom(type));
     }
 
     @Test
