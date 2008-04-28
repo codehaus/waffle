d262854fcabcfb9c208d32e4dc2ffd981c27287f
mauro <mauro@88262fed-822f-0410-898e-89d428b5ecb3>
2008-04-28 10:31:41 +0000

    Added copyright.

--- waffle-core/src/main/java/org/codehaus/waffle/bind/DefaultStringTransmuter.java
+++ waffle-core/src/main/java/org/codehaus/waffle/bind/DefaultStringTransmuter.java
@@ -1,8 +1,20 @@
+/*****************************************************************************
+ * Copyright (c) 2005-2008 Michael Ward                                      *
+ * All rights reserved.                                                      *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE.txt file.                                                     *
+ *                                                                           *
+ * Original code by: Michael Ward                                            *
+ *****************************************************************************/
 package org.codehaus.waffle.bind;
 
 /**
  * This implementation uses the {@link org.codehaus.waffle.bind.ValueConverterFinder} and its resulting
  * {@link ValueConverter} to transform a String value into the specified type.
+ * 
+ * @author Michael Ward
  */
 public class DefaultStringTransmuter implements StringTransmuter {
     private final ValueConverterFinder valueConverterFinder;
