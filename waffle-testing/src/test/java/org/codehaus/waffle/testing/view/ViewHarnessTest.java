4d17aac92a9f985d407565af500e5281820bf7b9
mauro <mauro@88262fed-822f-0410-898e-89d428b5ecb3>
2008-05-20 13:34:15 +0000

    WAFFLE-81:  Added decorator data model to override any entry of processor data model.

--- waffle-testing/src/test/java/org/codehaus/waffle/testing/view/ViewHarnessTest.java
+++ waffle-testing/src/test/java/org/codehaus/waffle/testing/view/ViewHarnessTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 
 import org.codehaus.waffle.testing.ListController;
 import org.junit.Test;
@@ -28,7 +29,7 @@ public class ViewHarnessTest {
     public void canDecorateWithSitemesh() {
         ListController controller = new ListController();
         controller.list();
-        String decorated = decorateView("freemarker/list.ftl", controller, "WEB-INF/testing-decorators.xml", "layout");
+        String decorated = decorateView("freemarker/list.ftl", controller, "WEB-INF/testing-decorators.xml", "layout", new HashMap<String, Object>());
         assertTrue(decorated.length() > 0);
     }
 
@@ -40,7 +41,9 @@ public class ViewHarnessTest {
         exportView(processView("freemarker/list.ftl", controller, false), list);
         assertTrue(list.exists());
         File decorated = new File("target/export/decorated-list.html");
-        exportView(decorateView("freemarker/list.ftl", controller, "WEB-INF/testing-decorators.xml", "layout"), decorated);
+        HashMap<String, Object> decoratorDataModel = new HashMap<String, Object>();
+        decoratorDataModel.put("base", "..");
+        exportView(decorateView("freemarker/list.ftl", controller, "WEB-INF/testing-decorators.xml", "layout", decoratorDataModel), decorated);
         assertTrue(decorated.exists());
     }
 }
