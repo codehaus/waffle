888c12da243f181180e0a5745d197c64ecd6f0a8
mauro <mauro@88262fed-822f-0410-898e-89d428b5ecb3>
2008-09-03 13:26:21 +0000

    More readable.

--- trunk/waffle-ruby/src/main/java/org/codehaus/waffle/context/pico/RubyScriptLoader.java
+++ trunk/waffle-ruby/src/main/java/org/codehaus/waffle/context/pico/RubyScriptLoader.java
@@ -3,12 +3,12 @@
  */
 package org.codehaus.waffle.context.pico;
 
+import javax.servlet.ServletContext;
+
+import org.codehaus.waffle.Startable;
 import org.jruby.Ruby;
 import org.jruby.javasupport.JavaEmbedUtils;
 import org.jruby.runtime.builtin.IRubyObject;
-import org.codehaus.waffle.Startable;
-
-import javax.servlet.ServletContext;
 
 public class RubyScriptLoader implements Startable {
     public static final String RUBY_SCRIPT_PATH_KEY = "org.codehaus.waffle.ruby.path";
@@ -21,23 +21,23 @@ public class RubyScriptLoader implements Startable {
     public RubyScriptLoader(ServletContext servletContext, Ruby runtime) {
         this.servletContext = servletContext;
         this.runtime = runtime;
+        this.rubyScriptPath = getScriptPath(servletContext);
+    }
 
+    private String getScriptPath(ServletContext servletContext) {
         String path = servletContext.getInitParameter(RUBY_SCRIPT_PATH_KEY);
-        
-        if(path == null) {
-            rubyScriptPath = DEFAULT_RUBY_SCRIPT_PATH;
-        } else {
-            rubyScriptPath = path;
+        if (path == null) {
+            return DEFAULT_RUBY_SCRIPT_PATH;
         }
+        return path;
     }
 
     public void start() {
-        runtime.getClassFromPath("Waffle::ScriptLoader")
-                .callMethod(runtime.getCurrentContext(), "load_all",
-                        new IRubyObject[]{
-                                JavaEmbedUtils.javaToRuby(runtime, rubyScriptPath),
-                                JavaEmbedUtils.javaToRuby(runtime, servletContext)
-                        });
+        runtime.getClassFromPath("Waffle::ScriptLoader").callMethod(
+                runtime.getCurrentContext(),
+                "load_all",
+                new IRubyObject[] { JavaEmbedUtils.javaToRuby(runtime, rubyScriptPath),
+                        JavaEmbedUtils.javaToRuby(runtime, servletContext) });
     }
 
     public void stop() {
