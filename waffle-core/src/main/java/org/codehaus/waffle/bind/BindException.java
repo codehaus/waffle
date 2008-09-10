848a6e23e2a26ee1e2f3e6262fae2118fe9ef293
mauro <mauro@88262fed-822f-0410-898e-89d428b5ecb3>
2008-09-10 11:46:01 +0000

    Removed message-less constructors, which would not provide a clear explanation of binding failure.

--- waffle-core/src/main/java/org/codehaus/waffle/bind/BindException.java
+++ waffle-core/src/main/java/org/codehaus/waffle/bind/BindException.java
@@ -5,10 +5,13 @@ package org.codehaus.waffle.bind;
 
 import org.codehaus.waffle.WaffleException;
 
+/**
+ * Thrown when unable bind values 
+ *
+ * @author Mike Ward
+ */
 @SuppressWarnings("serial")
 public class BindException extends WaffleException {
-    public BindException() {
-    }
 
     public BindException(String message) {
         super(message);
@@ -17,8 +20,5 @@ public class BindException extends WaffleException {
     public BindException(String message, Throwable cause) {
         super(message, cause);
     }
-
-    public BindException(Throwable cause) {
-        super(cause);
-    }
+    
 }
