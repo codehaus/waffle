aefa3d033345551f390ff3defbbe2be5f2b66418
mauro <mauro@88262fed-822f-0410-898e-89d428b5ecb3>
2008-09-15 17:54:28 +0000

    WAFFLE-96:  Added I18N support to action method interceptors.

--- trunk/waffle-core/src/main/java/org/codehaus/waffle/action/intercept/SecurityMethodInterceptor.java
+++ trunk/waffle-core/src/main/java/org/codehaus/waffle/action/intercept/SecurityMethodInterceptor.java
@@ -3,44 +3,61 @@
  */
 package org.codehaus.waffle.action.intercept;
 
-import org.codehaus.waffle.action.ActionMethodInvocationException;
-import org.codehaus.waffle.action.annotation.ActionMethod;
-import org.codehaus.waffle.controller.ControllerDefinition;
-
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+import org.codehaus.waffle.action.ActionMethodInvocationException;
+import org.codehaus.waffle.action.annotation.ActionMethod;
+import org.codehaus.waffle.controller.ControllerDefinition;
+import org.codehaus.waffle.i18n.DefaultMessageResources;
+import org.codehaus.waffle.i18n.MessageResources;
+
 /**
- * <p>This interceptor ensure that only {@link ActionMethod} annotated methods are invokable as {@code Actions}.
- * Usage of this will help protect your application against malicious attacks.
+ * <p>
+ * This interceptor ensure that only {@link ActionMethod} annotated methods are invokable as {@code Actions}. Usage of
+ * this will help protect your application against malicious attacks.
+ * </p>
+ * 
+ * @author Micheal Ward
  */
 public class SecurityMethodInterceptor implements MethodInterceptor {
 
+    private final MessageResources messageResources;
+
+    public SecurityMethodInterceptor() {
+        this(new DefaultMessageResources());
+    }
+
+    public SecurityMethodInterceptor(MessageResources messageResources) {
+        this.messageResources = messageResources;
+    }
+
     /**
-     * Will always return true (intercepts ALL action methods)
-     *
-     * {@inheritDoc}
+     * Will always return true (intercepts ALL action methods) {@inheritDoc}
      */
     public boolean accept(Method actionMethod) {
         return true; // intercept all!!!!
     }
 
     /**
-     * Ensure that the action method tobe invoked is annotated with the {@link ActionMethod} annotation.  If no annotation
-     * is present a {@link ActionMethodInvocationException} will be thrown.
-     *
+     * <p>
+     * Ensure that the action method to be invoked is annotated with the {@link ActionMethod} annotation. If no
+     * annotation is present a {@link ActionMethodInvocationException} will be thrown.
+     * </p>
+     * <p>
      * {@inheritDoc}
+     * </p>
      */
-    public Object intercept(ControllerDefinition controllerDefinition,
-                            Method method,
-                            InterceptorChain chain,
-                            Object... arguments) throws IllegalAccessException, InvocationTargetException {
+    public Object intercept(ControllerDefinition controllerDefinition, Method method, InterceptorChain chain,
+            Object... arguments) throws IllegalAccessException, InvocationTargetException {
         if (method.isAnnotationPresent(ActionMethod.class)) {
             return chain.proceed(controllerDefinition, method, arguments);
         }
 
         // Only notify that the requested action could not invoked do NOT give any detailed information (Security Risk)
-        throw new ActionMethodInvocationException("Requested action method cannot be invoke remotely.");
+        String message = messageResources.getMessageWithDefault("actionMethodCannotBeInvoked",
+                "Action method cannot be invoked");
+        throw new ActionMethodInvocationException(message);
     }
 
 }
