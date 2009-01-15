429335faadff944a953534f0cc98b289754a9222
paul <paul@88262fed-822f-0410-898e-89d428b5ecb3>
2009-01-15 22:52:27 +0000

    half working

--- sandbox/v2experiment/examples/paranamer-example/src/main/java/org/codehaus/waffle/example/paranamer/ParanamerExampleWebappComposer.java
+++ sandbox/v2experiment/examples/paranamer-example/src/main/java/org/codehaus/waffle/example/paranamer/ParanamerExampleWebappComposer.java
@@ -5,6 +5,10 @@ import org.codehaus.waffle.example.paranamer.action.HelloWorldController;
 import org.codehaus.waffle.example.paranamer.action.PersonController;
 import org.codehaus.waffle.example.paranamer.dao.SimplePersonDAO;
 import org.codehaus.waffle.context.WaffleWebappComposer;
+import org.codehaus.waffle.action.AnnotatedMethodDefinitionFinder;
+import org.codehaus.waffle.action.MethodDefinitionFinder;
+import org.codehaus.waffle.action.ParanamerMethodDefinitionFinder;
+import org.codehaus.waffle.bind.converters.DateValueConverter;
 import org.picocontainer.MutablePicoContainer;
 
 import javax.servlet.ServletContext;
@@ -14,10 +18,17 @@ public class ParanamerExampleWebappComposer extends WaffleWebappComposer {
     @Override
     public void composeApplication(MutablePicoContainer picoContainer, ServletContext servletContext) {
         super.composeApplication(picoContainer, servletContext);
+
+        picoContainer.addComponent(DateValueConverter.class);
+
         picoContainer.addComponent(SimplePersonDAO.class);
         picoContainer.addComponent("helloworld", HelloWorldController.class);
         picoContainer.addComponent("ajaxexample", AjaxExample.class);
         picoContainer.addComponent("people/person", PersonController.class);
+    }
+
+    protected Class<? extends MethodDefinitionFinder> methodDefinitionFinder() {
+        return ParanamerMethodDefinitionFinder.class;
     }
 
     @Override
