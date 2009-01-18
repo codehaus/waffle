6a78bbdf2487cbf7720138c0ff4a8439fa7b6113
mauro <mauro@88262fed-822f-0410-898e-89d428b5ecb3>
2009-01-18 14:54:26 +0000

    Working through example issues.

--- sandbox/v2experiment/examples/paranamer-example/src/main/java/org/codehaus/waffle/example/paranamer/AjaxExample.java
+++ sandbox/v2experiment/examples/paranamer-example/src/main/java/org/codehaus/waffle/example/paranamer/AjaxExample.java
@@ -31,14 +31,14 @@ public class AjaxExample {
         AjaxExample.Item item = available.remove(index);
         selected.add(item);
 
-        return new View("selection.jspx", this);
+        return new View("selection.jspx");
     }
 
     public View remove(int index) {
         AjaxExample.Item item = selected.remove(index);
         available.add(item);
 
-        return new View("selection.jspx", this);
+        return new View("selection.jspx");
     }
 
     public class Item {
--- sandbox/v2experiment/examples/paranamer-example/src/main/java/org/codehaus/waffle/example/paranamer/ParanamerExampleWebappComposer.java
+++ sandbox/v2experiment/examples/paranamer-example/src/main/java/org/codehaus/waffle/example/paranamer/ParanamerExampleWebappComposer.java
@@ -1,26 +1,23 @@
 package org.codehaus.waffle.example.paranamer;
 
+import javax.servlet.ServletContext;
+
+import org.codehaus.waffle.action.MethodDefinitionFinder;
+import org.codehaus.waffle.action.ParanamerMethodDefinitionFinder;
+import org.codehaus.waffle.bind.converters.DateValueConverter;
+import org.codehaus.waffle.context.pico.WaffleWebappComposer;
 import org.codehaus.waffle.example.paranamer.action.CalculatorController;
 import org.codehaus.waffle.example.paranamer.action.HelloWorldController;
 import org.codehaus.waffle.example.paranamer.action.PersonController;
 import org.codehaus.waffle.example.paranamer.dao.SimplePersonDAO;
-import org.codehaus.waffle.context.pico.WaffleWebappComposer;
-import org.codehaus.waffle.action.AnnotatedMethodDefinitionFinder;
-import org.codehaus.waffle.action.MethodDefinitionFinder;
-import org.codehaus.waffle.action.ParanamerMethodDefinitionFinder;
-import org.codehaus.waffle.bind.converters.DateValueConverter;
 import org.picocontainer.MutablePicoContainer;
-
-import javax.servlet.ServletContext;
 
 public class ParanamerExampleWebappComposer extends WaffleWebappComposer {
 
     @Override
     public void composeApplication(MutablePicoContainer picoContainer, ServletContext servletContext) {
         super.composeApplication(picoContainer, servletContext);
-
         picoContainer.addComponent(DateValueConverter.class);
-
         picoContainer.addComponent(SimplePersonDAO.class);
         picoContainer.addComponent("helloworld", HelloWorldController.class);
         picoContainer.addComponent("ajaxexample", AjaxExample.class);
