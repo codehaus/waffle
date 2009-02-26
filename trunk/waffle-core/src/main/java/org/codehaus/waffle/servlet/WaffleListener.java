10a6c7fcfe18289cb0280c245004e91402c02fd9
paul <paul@88262fed-822f-0410-898e-89d428b5ecb3>
2009-02-26 18:42:17 +0000

    upgrade picocontainer + web + xstream

--- trunk/waffle-core/src/main/java/org/codehaus/waffle/servlet/WaffleListener.java
+++ trunk/waffle-core/src/main/java/org/codehaus/waffle/servlet/WaffleListener.java
@@ -12,17 +12,12 @@ import org.picocontainer.web.PicoServletContainerListener;
 public class WaffleListener extends PicoServletContainerListener {
 
     protected ScopedContainers makeScopedContainers() {
-        DefaultPicoContainer application = new DefaultPicoContainer(new Guarding().wrap(new Caching()), makeLifecycleStrategy(), makeParentContainer());
+        DefaultPicoContainer application = new DefaultPicoContainer(new Guarding().wrap(new Caching()), new WaffleLifecycleStrategy(makeAppComponentMonitor()), makeParentContainer());
         Storing sessionStoring = new Storing();
-        DefaultPicoContainer session = new DefaultPicoContainer(new Guarding().wrap(sessionStoring), makeLifecycleStrategy(), application);
+        DefaultPicoContainer session = new DefaultPicoContainer(new Guarding().wrap(sessionStoring), new WaffleLifecycleStrategy(makeSessionComponentMonitor()), application);
         Storing requestStoring = new Storing();
-        DefaultPicoContainer request = new DefaultPicoContainer(new Guarding().wrap(addRequestBehaviors(requestStoring)), makeLifecycleStrategy(), session);
+        DefaultPicoContainer request = new DefaultPicoContainer(new Guarding().wrap(addRequestBehaviors(requestStoring)), new WaffleLifecycleStrategy(makeRequestComponentMonitor()), session);
         return new ScopedContainers(application, session, request, sessionStoring, requestStoring);
     }
-
-    protected LifecycleStrategy makeLifecycleStrategy() {
-        return new WaffleLifecycleStrategy(makeComponentMonitor());
-    }
-
 
 }
