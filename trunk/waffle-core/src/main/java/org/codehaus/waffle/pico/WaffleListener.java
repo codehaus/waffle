1b55e9fc5a2e5426c82ede014183c4609b593b72
paul <paul@88262fed-822f-0410-898e-89d428b5ecb3>
2009-04-08 20:15:10 +0000

    upgrade to latest pico

--- trunk/waffle-core/src/main/java/org/codehaus/waffle/pico/WaffleListener.java
+++ trunk/waffle-core/src/main/java/org/codehaus/waffle/pico/WaffleListener.java
@@ -6,17 +6,23 @@ import org.picocontainer.behaviors.Caching;
 import org.picocontainer.behaviors.Guarding;
 import org.picocontainer.behaviors.Storing;
 import org.picocontainer.web.PicoServletContainerListener;
+import org.picocontainer.web.ScopedContainers;
+import org.picocontainer.web.ThreadLocalLifecycleState;
 
 @SuppressWarnings("serial")
 public class WaffleListener extends PicoServletContainerListener {
 
     protected ScopedContainers makeScopedContainers() {
-        DefaultPicoContainer application = new DefaultPicoContainer(new Guarding().wrap(new Caching()), new WaffleLifecycleStrategy(makeAppComponentMonitor()), makeParentContainer());
+        DefaultPicoContainer application = new DefaultPicoContainer(new Guarding().wrap(new Caching()), new WaffleLifecycleStrategy(makeAppComponentMonitor()), makeParentContainer(), makeAppComponentMonitor());
         Storing sessionStoring = new Storing();
-        DefaultPicoContainer session = new DefaultPicoContainer(new Guarding().wrap(sessionStoring), new WaffleLifecycleStrategy(makeSessionComponentMonitor()), application);
+        DefaultPicoContainer session = new DefaultPicoContainer(new Guarding().wrap(sessionStoring), new WaffleLifecycleStrategy(makeSessionComponentMonitor()), application, makeSessionComponentMonitor());
         Storing requestStoring = new Storing();
-        DefaultPicoContainer request = new DefaultPicoContainer(new Guarding().wrap(addRequestBehaviors(requestStoring)), new WaffleLifecycleStrategy(makeRequestComponentMonitor()), session);
-        return new ScopedContainers(application, session, request, sessionStoring, requestStoring);
+        DefaultPicoContainer request = new DefaultPicoContainer(new Guarding().wrap(addRequestBehaviors(requestStoring)), new WaffleLifecycleStrategy(makeRequestComponentMonitor()), session, makeRequestComponentMonitor());
+        ThreadLocalLifecycleState sessionState = new ThreadLocalLifecycleState();
+        ThreadLocalLifecycleState requestState = new ThreadLocalLifecycleState();
+        session.setLifecycleState(sessionState);
+        request.setLifecycleState(requestState);
+        return new ScopedContainers(application, session, request, sessionStoring, requestStoring, sessionState, requestState);
     }
 
 }
